777d13cfa4909b701e669d25ea713b1cf67d5ebd
Eugene Todoruk <nighteugene@ya.ru>
2025-10-19 15:13:36 +0300

    [hostinfo] Support AuroraOS

--- hostinfo.lua
+++ hostinfo.lua
@@ -3,7 +3,7 @@ local api = {}
 local os_string = love.system.getOS()
 
 function api.isTouchDevice()
-	return os_string == 'Android' or os_string == 'iOS'
+	return os_string == 'Android' or os_string == 'iOS' or os_string == 'AuroraOS'
 end
 
 return api
